dcdfe0ecfb7f6bbe3d010cca0bc243f5951066e9
Nick Espig <nickespig@gmail.com>
2019-02-07 23:02:32 +0100

    Install image-match

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import find_packages, setup
 
 version = '0.2.2.dev0'
-
 
 setup(name='XenianChannelBot',
       version=version,
@@ -25,6 +23,7 @@ setup(name='XenianChannelBot',
           'mr.developer',
           'mongoengine',
           'python-telegram-bot',
+          'image_match',
       ],
 
       entry_points={
